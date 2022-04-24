0ff3b502d15f1a9ed0b213b769aba591145186a8
andrewKievUA <andrew1990mk@gmail.com>
2022-04-24 10:47:36 +0300

    homepage add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import HomePage from './pages/HomePage/HomePage'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-
+        <HomePage/>
     </div>
   );
 }
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,46 @@
+import "./HomePageStyles.scss"
+
+const HomePage =()=>(
+    <div className="HomePage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Jakets</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Sneakers</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMENS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MENS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+)
+
+
+export default HomePage
